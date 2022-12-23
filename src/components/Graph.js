3b60a05e52635a30f702e10b2e8434e630bf751d
Fiewor <johnfiewor@gmail.com>
2022-12-23 22:57:33 +0100

    feat: implement line graph with dummy data

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,8 +1,76 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { P } from "./CoinList";
+Chart.register(CategoryScale);
 
 const Graph = () => {
-  return <Container>Graph</Container>;
+  const { list, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.details
+  );
+  if (isLoading) {
+    return <P>Loading...</P>;
+  }
+
+  if (isError) {
+    return <P>Error: {message}</P>;
+  }
+  if (isSuccess) {
+    const {
+      market_data: {
+        current_price: { usd },
+        price_change_percentage_24h: change,
+      },
+    } = list;
+
+    console.log("graph list: ", list);
+    return (
+      <Container>
+        <Top>
+          <Price>
+            {new Intl.NumberFormat(`en-US`, {
+              currency: `USD`,
+              style: "currency",
+            }).format(usd)}
+          </Price>
+          <Change>{change}</Change>
+        </Top>
+        <Bottom>
+          <Line
+            data={{
+              // x-axis label values
+              labels: [
+                "Nov",
+                "Dec",
+                "Jan",
+                "Feb",
+                "Mar",
+                "Apr",
+                "May",
+                "June",
+                "Jul",
+              ],
+              datasets: [
+                {
+                  label: "",
+                  // y-axis data plotting values
+                  data: [0, 10, 20, 30, 40, 50, 60],
+                  fill: false,
+                  borderWidth: 3,
+                  backgroundColor: "#3d3942",
+                  borderColor: "#2b60b3",
+                  responsive: true,
+                },
+              ],
+            }}
+          />
+        </Bottom>
+      </Container>
+    );
+  }
 };
 
 const Container = styled.div`
@@ -10,6 +78,34 @@ const Container = styled.div`
   height: 60vh;
   background: black;
   border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  color: white;
+`;
+
+const Top = styled.div`
+  width: 30%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 20%;
+`;
+
+const Bottom = styled.div`
+  width: 100%;
+  height: 80%;
+  display: flex;
+  padding: 1rem;
+  align-items: flex-end;
+`;
+
+const Price = styled.h2`
+  padding: 0.5rem 1rem;
+`;
+
+const Change = styled.p`
+  color: lightgreen;
 `;
 
 export default Graph;
